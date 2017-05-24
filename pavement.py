c3837be159124d8ec30239649b27ebc2de02c91d
Christian Fobel <christian@fobel.net>
2017-05-24 18:17:07 -0400

    Sort imports, update URL, add notebook requirement

--- pavement.py
+++ pavement.py
@@ -14,9 +14,9 @@ setup(name='ipython-helpers',
       version=version.getVersion(),
       author='Christian Fobel',
       author_email='christian@fobel.net',
-      url='https://github.com/cfobel/ipython-helpers',
+      url='https://github.com/wheeler-microfluidics/ipython-helpers',
       license='BSD',
-      install_requires=['IPython>=2.0', 'path_helpers>=0.2'],
+      install_requires=['notebook', 'IPython>=2.0', 'path_helpers>=0.2'],
       packages=['ipython_helpers'])
 
 @task
